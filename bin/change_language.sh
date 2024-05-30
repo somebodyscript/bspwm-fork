85934c9e2d3a24b396ac148730ee6bd6d9cd26c7
some.js <144458220+somebodyscript@users.noreply.github.com>
2024-05-30 20:54:59 +0200

    Update change_language.sh

--- bin/change_language.sh
+++ bin/change_language.sh
@@ -1,10 +1,13 @@
-#! /bin/bash
+#!/bin/bash
 
 CURRENT_LAYOUT=$(setxkbmap -query | awk -F : 'NR==3{print $2}' | sed 's/ //g')
 
 if [ "$CURRENT_LAYOUT" = "us" ]; then
     setxkbmap "ru"
-	notify-send "Lang: RU" -t 700
+    notify-send "Lang: RU" -t 700
+elif [ "$CURRENT_LAYOUT" = "ru" ]; then
+    setxkbmap "ua"
+    notify-send "Lang: UA" -t 700
 else
     setxkbmap "us"
     notify-send "Lang: US" -t 700
